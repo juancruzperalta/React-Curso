aa7aa2b1841375ca9864b4068a50a4b4b2d38dca
juancruzperalta <juanc7peralta@gmail.com>
2023-01-31 19:35:53 -0300

    31-01-2023 React

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=alW3EcmnzghVc5aMWVvF5N7c5tKInMyp&q=valorant=${category}&limit=10`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+  return gifs;
+};
